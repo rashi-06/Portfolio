99a5958345d9c937c7b3d536d79e8acd2b5d41cf
rashi-06 <rashisharma18062001@gmail.com>
2023-08-11 23:45:31 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router } from "react-router-dom";
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+
+function App() {
+  
+  return (
+    <Router>
+      <div className="relative bg-primary z-0">
+        <div className="bg-hero-pattern bg-no-repeat bg-center bg-cover ">
+            <Navbar/>
+            <Hero />
+        </div>
+        <About/>
+        <Works/>
+        <Tech />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas/>
+        </div>
+      </div>
+        
+    </Router>
+  )
+}
+
+export default App
