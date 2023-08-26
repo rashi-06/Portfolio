0db0a217e36d5e617a42c4f08d41ddc966e8c2e0
rashi-06 <rashisharma18062001@gmail.com>
2023-08-26 22:17:46 +0530

    ec

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
       <div>Instructions</div>
       <h1>Observations.</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim voluptas illum, voluptatem suscipit optio autem accusamus doloribus fugiat impedit tenetur minus amet voluptatibus!</p>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed laborum, nihil nam minus vel maiores quia hic? Saepe quia aperiam sed tenetur sunt, inventore molestiae, ducimus nam cumque, nisi numquam!</p>
+
     </motion.div>
   </>
   )
