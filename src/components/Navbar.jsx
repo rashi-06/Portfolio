5263c16f1c550ba5e338047543c511c220968b81
rashi-06 <rashisharma18062001@gmail.com>
2023-09-11 01:00:05 +0530

    created contact component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,78 +1,102 @@
-import React , {useEffect , useState} from 'react'
-import { Link } from 'react-router-dom'
-import { navLinks } from '../constants'
-import { styles } from '../styles'
-import {logo , menu , close} from "../assets"
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
+import { styles } from "../styles";
+import { navLinks } from "../constants";
+import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
-
   const [active, setActive] = useState("");
-  const [toggle , setToggle] = useState(false);
+  const [toggle, setToggle] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      if (scrollTop > 100) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
-    <nav className={`${
-      styles.paddingX
-    } w-full flex items-center py-5 fixed top-0 z-20
-      bg-primary
-    }`}>
-       <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
+    <nav
+      className={`${
+        styles.paddingX
+      } w-full flex items-center py-5 fixed top-0 z-20 ${
+        scrolled ? "bg-primary" : "bg-transparent"
+      }`}
+    >
+      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
         <Link
           to='/'
           className='flex items-center gap-2'
           onClick={() => {
             setActive("");
-            // window.scrollTo(0, 0);
+            window.scrollTo(0, 0);
           }}
         >
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
-            Rashi Sharma
+            Adrian &nbsp;
+            <span className='sm:block hidden'> | JavaScript Mastery</span>
           </p>
         </Link>
 
         <ul className='list-none hidden sm:flex flex-row gap-10'>
-          {navLinks.map((ele)=>(
-              <li key={ele.id}
-                className={`${active === ele.title ? "text-white" : "text-secondary" }  hover:text-white cursor-pointer text-[18px] font-medium ` }>
-                <Link to={`/${ele.id}`}
-                  onClick={()=>setActive(ele.title)}
-                >{ele.title}</Link>
-              </li>
+          {navLinks.map((nav) => (
+            <li
+              key={nav.id}
+              className={`${
+                active === nav.title ? "text-white" : "text-secondary"
+              } hover:text-white text-[18px] font-medium cursor-pointer`}
+              onClick={() => setActive(nav.title)}
+            >
+              <a href={`#${nav.id}`}>{nav.title}</a>
+            </li>
           ))}
         </ul>
 
-          {/* //mobile view */}
-          <div className='sm:hidden flex flex-1 justify-end items-center'>
-            <img
-              src={toggle ? close : menu}
-              alt='menu'
-              className='w-[28px] h-[28px] object-contain'
-              onClick={() => setToggle(!toggle)}
-            />
+        <div className='sm:hidden flex flex-1 justify-end items-center'>
+          <img
+            src={toggle ? close : menu}
+            alt='menu'
+            className='w-[28px] h-[28px] object-contain'
+            onClick={() => setToggle(!toggle)}
+          />
 
-
-            <div
-              className={`${!toggle ? "hidden" : "flex"} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
-            >
-              <ul className='list-none flex flex-col justify-end items-start flex-1 gap-4'>
-              {navLinks.map((ele)=>(
-                <li key={ele.id}
-                  className={`${active === ele.title ? "text-white" : "text-secondary" }  font-poppins cursor-pointer text-[16px] font-medium ` }>
-                  <Link to={`/${ele.id}`}
-                    onClick={()=>{
-                      setActive(ele.title);
-                      setToggle(!toggle);
-                    }}
-                  >{ele.title}</Link>
+          <div
+            className={`${
+              !toggle ? "hidden" : "flex"
+            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+          >
+            <ul className='list-none flex justify-end items-start flex-1 flex-col gap-4'>
+              {navLinks.map((nav) => (
+                <li
+                  key={nav.id}
+                  className={`font-poppins font-medium cursor-pointer text-[16px] ${
+                    active === nav.title ? "text-white" : "text-secondary"
+                  }`}
+                  onClick={() => {
+                    setToggle(!toggle);
+                    setActive(nav.title);
+                  }}
+                >
+                  <a href={`#${nav.id}`}>{nav.title}</a>
                 </li>
               ))}
-              </ul>
-            </div>
+            </ul>
           </div>
+        </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -8,6 +8,48 @@ import { projects } from '../constants'
 import { fadeIn, textVariant } from '../utils/motion'
 
 
+const ProjectCard = ({index , name , tags , image , description , source_code_link}) =>{
+    return(
+      <motion.div variants={fadeIn("up" , "spring" , index*0.5 , 0.75 )}>
+        <Tilt 
+          option={
+            {
+              max : 45 ,
+              scale : 1 ,
+              speed : 450 
+            }
+          } 
+          className= "bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full" 
+        >
+          <div className='relative w-full h-[230px]'>
+            <img
+              src={image}
+              alt={name}
+              className='w-full h-full object-cover rounded-2xl ' />
+
+            <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
+              <div onClick={()=>window.open(source_code_link , "_blank")} className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'>
+                <img src={github} alt='github' className='w-1/2 h-1/2 object-contain'/>
+              </div>
+            </div>
+          </div>
+          
+          <div className='mt-5 '>
+            <h3 className='text-white font-bold text-center'>{name}</h3>
+            <p className='mt-2 text-secondary text-[14px]'>{description}</p>
+          </div>
+          
+          <div className='mt-4 flex flex-wrap gap-1'>
+            {tags.map((tag)=>(
+              <p key = {tag.name} className={`text-[14px] ${tag.color}`}>#{tag.name}</p>
+            ))}
+          </div>
+
+        </Tilt>
+      </motion.div>
+    )
+}
+
 const Works = () => {
   return (
     <>
@@ -15,6 +57,20 @@ const Works = () => {
         <p className={styles.sectionSubText}>Projects</p>
         <h2 className={styles.sectionHeadText}>My Projects</h2>
       </motion.div>
+
+      <div className='w-full  flex'> 
+        <motion.p
+          variants={fadeIn("", "" , 0.1 , 1)}
+          className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'  >
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, vero? Cum nesciunt distinctio nisi quis laborum temporibus necessitatibus esse alias illo corporis eum dolorem, fugiat molestias beatae nostrum accusantium ab ullam odio sapiente eius voluptas non. Eaque quibusdam perspiciatis ipsam sapiente adipisci provident ea ad cupiditate placeat aliquam reiciendis mollitia optio est illum veritatis ut, laboriosam tempora libero expedita enim accusamus? Vero alias omnis, aspernatur dolore vitae fuga, sapiente necessitatibus quasi soluta laudantium quae aut voluptas, tempore earum numquam? Cum illo suscipit minus voluptatibus, reiciendis fuga assumenda praesentium!
+        </motion.p>
+      </div>
+
+      <div className='mt-20 flex flex-wrap gap-7'>
+        {projects.map((project , index)=>(
+            <ProjectCard  key = {index}  index= {index}  {...project} />
+        ))}
+      </div>
     </>
   )
 }
